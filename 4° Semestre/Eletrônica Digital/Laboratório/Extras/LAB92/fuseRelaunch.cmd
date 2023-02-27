24e6ff66ca812fa08dc3a43489ecadc7c415e40d
dsbrito <80075307+DsBrito@users.noreply.github.com>
2023-02-27 18:52:36 -0300

    ⚡ add :: extra - laboratórios

---
+++ 4° Semestre/Eletrônica Digital/Laboratório/Extras/LAB92/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "C:/Users/diona/OneDrive/Documentos/Projetos Xilinx/LAB92/mult_seq_isim_beh.exe" -prj "C:/Users/diona/OneDrive/Documentos/Projetos Xilinx/LAB92/mult_seq_beh.prj" "work.mult_seq" 
